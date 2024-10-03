2e5381244750b46b89d8cbdad45f942b928777ee
Midhun S <139331462+midhunsomu@users.noreply.github.com>
2024-10-03 15:15:50 +0530

    Update Histogram processing of an image.py

--- Histogram processing of an image.py
+++ Histogram processing of an image.py
@@ -42,6 +42,76 @@ colorscale=cv2.imread("color image of flower.jpg")
 g=cv2.resize(gi,(500,400))
 equ=cv2.equalizeHist(gi)
 
+import cv2
+import matplotlib.pyplot as plt
+gray_image = cv2.imread(r"C:\Users\admin\Downloads\gray--gray.jpg", cv2.IMREAD_GRAYSCALE)  
+color_image = cv2.imread(r"C:\Users\admin\Downloads\gray .jpg") 
+cv2.imshow("Gray Image",gray_image)
+cv2.imshow("Color Image",color_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+import cv2
+import matplotlib.pyplot as plt
+
+gray_image = cv2.imread(r"C:\Users\admin\Downloads\gray--gray.jpg", cv2.IMREAD_GRAYSCALE)  
+color_image = cv2.imread(r"C:\Users\admin\Downloads\gray .jpg") 
+
+gray_hist = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
+color_hist = cv2.calcHist([color_image], [0], None, [256], [0, 256])
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Grayscale Histogram")
+plt.xlabel("Pixel Intensity")
+plt.ylabel("Frequency")
+plt.plot(gray_hist, color='black')
+
+plt.subplot(1, 2, 2)
+plt.title("Color Histogram")
+plt.xlabel("Pixel Intensity")
+plt.ylabel("Frequency")
+plt.plot(color_hist, color='blue')
+
+plt.tight_layout()
+plt.show()
+
+import cv2
+import matplotlib.pyplot as plt
+gray_image = cv2.imread(r"C:\Users\admin\Downloads\gray--gray.jpg", cv2.IMREAD_GRAYSCALE)
+color_image = cv2.imread(r"C:\Users\admin\Downloads\gray .jpg")
+resized_gray_image = cv2.resize(gray_image, (500, 400))
+equalized_image = cv2.equalizeHist(gray_image)
+cv2.imshow("Original Grayscale Image", gray_image)
+cv2.waitKey(0)
+
+cv2.imshow("Resized Grayscale Image", resized_gray_image)
+cv2.waitKey(0)
+cv2.imshow("Equalized Grayscale Image", equalized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.figure()
+
+plt.subplot(1, 2, 1)
+plt.title("Original Grayscale Histogram")
+plt.xlabel("Pixel Intensity")
+plt.ylabel("Frequency")
+gray_hist = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
+plt.plot(gray_hist)
+
+plt.subplot(1, 2, 2)
+plt.title("Equalized Grayscale Histogram")
+plt.xlabel("Pixel Intensity")
+plt.ylabel("Frequency")
+equalized_hist = cv2.calcHist([equalized_image], [0], None, [256], [0, 256])
+plt.plot(equalized_hist)
+
+plt.tight_layout()
+plt.show()
+
+
 
 
 
